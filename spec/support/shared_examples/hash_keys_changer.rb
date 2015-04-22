9b349ff1db7ee66239c2bf80a028e91ca97db73e
Fernando Favini <darthjee@gmail.com>
2015-04-22 16:20:41 -0300

    Correct hash usage inside method calls

--- spec/support/shared_examples/hash_keys_changer.rb
+++ spec/support/shared_examples/hash_keys_changer.rb
@@ -9,13 +9,13 @@ shared_examples 'a class with change_key method' do
 
       expect(original).to receive(:deep_dup).and_return(copy)
       expect(copy).to receive(:change_keys!)
-      original.change_keys({ recursive: true }) { |k| "foo_#{k}" }
+      original.change_keys(recursive: true) { |k| "foo_#{k}" }
     end
 
     it 'does not affects the original hash' do
       original = { 'a' => 1, b: 2, c: { d: 3, e: 4 }, f: [{ g: 5 }, { h: 6 }] }
       expected = { 'a' => 1, b: 2, c: { d: 3, e: 4 }, f: [{ g: 5 }, { h: 6 }] }
-      changed = original.change_keys({ recursive: true }) { |k| "foo_#{k}" }
+      original.change_keys(recursive: true) { |k| "foo_#{k}" }
       expect(original).to eq(expected)
     end
   end
@@ -26,7 +26,7 @@ shared_examples 'a class with change_key method' do
     it 'affects the original hash' do
       original = { 'a' => 1, b: 2, c: { d: 3, e: 4 }, f: [{ g: 5 }, { h: 6 }] }
       not_expected = { 'a' => 1, b: 2, c: { d: 3, e: 4 }, f: [{ g: 5 }, { h: 6 }] }
-      original.change_keys!({ recursive: true }) { |k| "foo_#{k}" }
+      original.change_keys!(recursive: true) { |k| "foo_#{k}" }
       expect(original).to_not eq(not_expected)
     end
   end
@@ -34,23 +34,23 @@ end
 
 shared_examples 'a mnethod that is able to change keys' do |method|
   it 'accepts block to change the keys' do
-    { 'a' => 1, b: 2 }.public_send(method){ |k| "foo_#{k}" }.should eq({ 'foo_a' => 1, 'foo_b' => 2 })
+    { 'a' => 1, b: 2 }.public_send(method) { |k| "foo_#{k}" }.should eq('foo_a' => 1, 'foo_b' => 2)
   end
 
   it 'accepts block to change the keys' do
-    { a: 1, 'b' => 2 }.public_send(method){ |k| "foo_#{k}".to_sym }.should eq({ foo_a: 1, foo_b: 2 })
+    { a: 1, 'b' => 2 }.public_send(method) { |k| "foo_#{k}".to_sym }.should eq(foo_a: 1, foo_b: 2)
   end
 
   it 'applies the block recursively' do
-    { 'a' => 1, b:  { c: 3, d: 4 } }.public_send(method){ |k| "foo_#{k}" }.should eq({ 'foo_a' => 1, 'foo_b' =>  { 'foo_c' => 3, 'foo_d' => 4 } })
+    { 'a' => 1, b:  { c: 3, d: 4 } }.public_send(method) { |k| "foo_#{k}" }.should eq('foo_a' => 1, 'foo_b' =>  { 'foo_c' => 3, 'foo_d' => 4 })
   end
 
   it 'applies the block recursively when passed in options' do
-    { 'a' => 1, b:  { c: 3, d: 4 } }.public_send(method, recursive: true) { |k| "foo_#{k}" }.should eq({ 'foo_a' => 1, 'foo_b' =>  { 'foo_c' => 3, 'foo_d' => 4 } })
+    { 'a' => 1, b:  { c: 3, d: 4 } }.public_send(method, recursive: true) { |k| "foo_#{k}" }.should eq('foo_a' => 1, 'foo_b' =>  { 'foo_c' => 3, 'foo_d' => 4 })
   end
 
   it 'does not apply the block recursively when passed in options' do
-    { 'a' => 1, b:  { c: 3, 'd' => 4 } }.public_send(method, recursive: false) { |k| "foo_#{k}" }.should eq({ 'foo_a' => 1, 'foo_b' =>  { c: 3, 'd' => 4 } })
+    { 'a' => 1, b:  { c: 3, 'd' => 4 } }.public_send(method, recursive: false) { |k| "foo_#{k}" }.should eq('foo_a' => 1, 'foo_b' =>  { c: 3, 'd' => 4 })
   end
 
   it 'apply recursion on many levels' do
