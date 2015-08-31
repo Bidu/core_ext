194a4980547aef7705d552c7536cd667ace6e0e2
darthjee favini <darthjee@gmail.com>
2015-08-31 17:57:15 -0300

    Add underscore keys to hash

--- spec/lib/hash_spec.rb
+++ spec/lib/hash_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Hash do
   it_behaves_like 'a class with change_key method'
   it_behaves_like 'a class with camlize_keys method'
+  it_behaves_like 'a class with underscore_keys method'
   it_behaves_like 'a class with append_keys method'
   it_behaves_like 'a class with change_kvalues method'
   it_behaves_like 'a class with remap method'
---
+++ spec/support/shared_examples/keys_underscorer.rb
@@ -0,0 +1,82 @@
+shared_examples 'a class with underscore_keys method basic' do |method|
+  describe :underscore_keys do
+    let(:expected) { { input_key: 'value' } }
+
+    context 'with camel case keys' do
+      let(:hash) { { inputKey: 'value' } }
+
+      it 'converts the keys to snake case' do
+        expect(hash.send(method)).to eq(expected)
+      end
+    end
+
+    context 'with string keys' do
+      let(:expected) { { 'input_key' => 'value' } }
+      let(:hash) { { 'InputKey' => 'value' } }
+
+      it 'converts the keys to snake case' do
+        expect(hash.send(method)).to eq(expected)
+      end
+    end
+
+    context 'with deep keys change' do
+      let(:expected) { { input_key: { inner_key: 'value' } } }
+      let(:hash) { { InputKey: { InnerKey: 'value' } } }
+
+      it 'converts the keys to snake case' do
+        expect(hash.send(method)).to eq(expected)
+      end
+    end
+
+    context 'with array keys change' do
+      let(:expected) { { input_key: [{ inner_key: 'value' }] } }
+      let(:hash) { { InputKey: [{ InnerKey: 'value' }] } }
+
+      it 'converts the keys to snake case' do
+        expect(hash.send(method)).to eq(expected)
+      end
+    end
+
+    context 'without recursive options' do
+      context 'with deep keys change' do
+        let(:expected) { { input_key: { InnerKey: 'value' } } }
+        let(:hash) { { InputKey: { InnerKey: 'value' } } }
+
+        it 'converts the keys to lower camel case' do
+          expect(hash.send(method, recursive: false)).to eq(expected)
+        end
+      end
+
+      context 'with array keys change' do
+        let(:expected) { { input_key: [{ InnerKey: 'value' }] } }
+        let(:hash) { { InputKey: [{ InnerKey: 'value' }] } }
+
+        it 'converts the keys to camle case' do
+          expect(hash.send(method, recursive: false)).to eq(expected)
+        end
+      end
+    end
+  end
+end
+
+shared_examples 'a class with underscore_keys! method' do
+  let(:hash) { { inputKey: 'value' } }
+
+  it_behaves_like 'a class with underscore_keys method basic', :underscore_keys!
+
+  it 'changes original hash' do
+    expect { hash.underscore_keys! }.to change { hash }
+  end
+end
+
+shared_examples 'a class with underscore_keys method' do
+  let(:hash) { { inputKey: 'value' } }
+
+  it_behaves_like 'a class with underscore_keys method basic', :underscore_keys
+  it_behaves_like 'a class with underscore_keys! method'
+
+  it 'does not change original hash' do
+    expect { hash.underscore_keys }.not_to change { hash }
+  end
+end
+
