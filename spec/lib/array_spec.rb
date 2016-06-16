9bcde2a23bab8b44fbbc4e34aa4e37a116f705a8
darthjee <darthjee@gmail.com>
2016-06-16 01:52:47 -0300

    Extract random spec

--- spec/lib/array_spec.rb
+++ spec/lib/array_spec.rb
@@ -92,19 +92,7 @@ describe Array do
   end
 
   describe '#random' do
-    let(:array) { [ 7, 5, 3 ] }
-
-    (0..2).each do |index|
-      context "when random returns #{index}" do
-        before do
-          allow_any_instance_of(Array).to receive(:rand).with(array.size) { index }
-        end
-
-        it 'returns the randomized index of the array' do
-          expect(array.random).to eq(array[index])
-        end
-      end
-    end
+    it_behaves_like 'a method that returns a random element'
   end
 
   describe '#map_and_select' do
