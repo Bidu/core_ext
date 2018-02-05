b260bc1f9bd3f8d66cc39d658c83100560f117ab
darthjee <darthjee@gmail.com>
2018-02-05 19:08:21 +0100

    Extract default value

--- lib/darthjee/core_ext/object.rb
+++ lib/darthjee/core_ext/object.rb
@@ -1,18 +1,6 @@
+require 'darthjee/core_ext/object/default_value'
+
 class Object
-  class << self
-    private
-
-    def default_value(name, value)
-      define_method(name) { |*_| value }
-    end
-
-    def default_values(*names, value)
-      names.each do |name|
-        default_value(name, value)
-      end
-    end
-  end
-
   def is_any?(*classes)
     classes.any? do |clazz|
       is_a?(clazz)
