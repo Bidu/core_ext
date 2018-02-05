c8855a51d0790c5900a038b43c18dee5b9bcd2d7
darthjee <darthjee@gmail.com>
2018-02-05 16:30:38 +0100

    Add default value method

--- spec/lib/object_spec.rb
+++ spec/lib/object_spec.rb
@@ -32,4 +32,32 @@ describe Object do
       end
     end
   end
+
+  describe '.default_value' do
+    subject { DefaultValue.new }
+
+    it 'accepts default value' do
+      expect(subject.x).to eq(10)
+    end
+
+    it do
+      expect(Object).not_to respond_to(:default_value)
+    end
+  end
+
+  describe '.default_values' do
+    subject { DefaultValue.new }
+
+    it 'accepts default values' do
+      expect(subject.y).to eq(20)
+    end
+
+    it 'accepts default values' do
+      expect(subject.z).to eq(20)
+    end
+
+    it do
+      expect(Object).not_to respond_to(:default_values)
+    end
+  end
 end
