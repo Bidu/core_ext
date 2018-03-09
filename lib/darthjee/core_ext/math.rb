47b2a17b126f34e1f18deaa3a897905b738e7b51
darthjee <darthjee@gmail.com>
2018-03-09 17:08:02 +0100

    Use single logic

--- lib/darthjee/core_ext/math.rb
+++ lib/darthjee/core_ext/math.rb
@@ -1,11 +1,9 @@
 module Math
   def self.average(values)
-    if values.is_a? Array
-      values.sum / values.size.to_f
-    else
-      values.inject(0) do |sum, vals|
-        sum + vals.inject { |a,b| a * b }
-      end / values.values.sum.to_f
-    end
+    values = values.map { |v| [v,1] }.to_h unless values.is_a?(Hash)
+
+    values.inject(0) do |sum, vals|
+      sum + vals.inject { |a,b| a * b }
+    end / values.values.sum.to_f
   end
 end
