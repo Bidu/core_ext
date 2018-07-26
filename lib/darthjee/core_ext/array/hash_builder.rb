f801559a7e46308295a11c45bdd8530ceed49c50
darthjee <darthjee@gmail.com>
2018-07-26 09:09:11 +0200

    Nesting fix

--- lib/darthjee/core_ext/array/hash_builder.rb
+++ lib/darthjee/core_ext/array/hash_builder.rb
@@ -1,20 +1,22 @@
-class Array::HashBuilder
-  attr_accessor :values, :keys
+class Array
+  class HashBuilder
+    attr_accessor :values, :keys
 
-  def initialize(values, keys)
-    @values = values.dup
-    @keys = keys.dup
-  end
+    def initialize(values, keys)
+      @values = values.dup
+      @keys = keys.dup
+    end
 
-  def build
-    fixes_sizes
+    def build
+      fixes_sizes
 
-    Hash[[keys, values].transpose]
-  end
+      Hash[[keys, values].transpose]
+    end
 
-  private
+    private
 
-  def fixes_sizes
-    values.concat Array.new(keys.size - values.size) if keys.size > values.size
+    def fixes_sizes
+      values.concat Array.new(keys.size - values.size) if keys.size > values.size
+    end
   end
 end
--- lib/darthjee/core_ext/hash/deep_hash_constructor.rb
+++ lib/darthjee/core_ext/hash/deep_hash_constructor.rb
@@ -1,83 +1,85 @@
-class Hash::DeepHashConstructor
-  attr_accessor :separator
+class Hash
+  class DeepHashConstructor
+    attr_accessor :separator
 
-  def initialize(separator)
-    @separator = separator
-  end
-
-  def deep_hash(object)
-    if object.is_a? Array
-      array_deep_hash(object)
-    elsif object.is_a? Hash
-      hash_deep_hash(object)
-    else
-      object
+    def initialize(separator)
+      @separator = separator
     end
-  end
 
-  private
-
-  def array_deep_hash(array)
-    array.map { |v| v.is_a?(Hash) ? deep_hash(v) : v }
-  end
-
-  def hash_deep_hash(hash)
-    {}.tap do |new_hash|
-      hash.each do |k, v|
-        base_key, child_key = split_key(k, separator)
-        set_deep_hash_positioned_value(new_hash, base_key, v, child_key)
-      end
-
-      new_hash.each do |k, v|
-        new_hash[k] = deep_hash(v)
+    def deep_hash(object)
+      if object.is_a? Array
+        array_deep_hash(object)
+      elsif object.is_a? Hash
+        hash_deep_hash(object)
+      else
+        object
       end
     end
-  end
 
-  def split_key(key, separator)
-    separator_rxp = (separator == '.') ? "\\#{separator}" : separator
-    skipper = "[^#{separator}]"
-    regexp = Regexp.new("^(#{skipper}*)#{separator_rxp}(.*)")
-    match = key.match(regexp)
+    private
 
-    match ? match[1..2] : key
-  end
-
-  def set_deep_hash_array_value(hash, base_key, index, value, key = nil)
-    key_without_index = base_key.gsub("[#{index}]", '')
-    hash[key_without_index] ||= []
-
-    if key.nil?
-      hash[key_without_index][index] = value
-    else
-      hash[key_without_index][index] ||= {}
-      hash[key_without_index][index][key] = value
+    def array_deep_hash(array)
+      array.map { |v| v.is_a?(Hash) ? deep_hash(v) : v }
     end
-  end
 
-  def set_deep_hash_positioned_value(new_hash, base_key, v, child_key)
-    index = array_index(base_key)
+    def hash_deep_hash(hash)
+      {}.tap do |new_hash|
+        hash.each do |k, v|
+          base_key, child_key = split_key(k, separator)
+          set_deep_hash_positioned_value(new_hash, base_key, v, child_key)
+        end
 
-    if index
-      set_deep_hash_array_value(new_hash, base_key, index, v, child_key)
-    else
-      set_deep_hash_value(new_hash, base_key, v, child_key)
+        new_hash.each do |k, v|
+          new_hash[k] = deep_hash(v)
+        end
+      end
     end
-  end
 
-  def array_index(key)
-    match = key.match(/\[([^)]+)\]/)
-    if match
-      match[1].to_i
+    def split_key(key, separator)
+      separator_rxp = (separator == '.') ? "\\#{separator}" : separator
+      skipper = "[^#{separator}]"
+      regexp = Regexp.new("^(#{skipper}*)#{separator_rxp}(.*)")
+      match = key.match(regexp)
+
+      match ? match[1..2] : key
     end
-  end
 
-  def set_deep_hash_value(hash, base_key, value, key = nil)
-    if key.nil?
-      hash[base_key] = value
-    else
-      hash[base_key] ||= {}
-      hash[base_key][key] = value
+    def set_deep_hash_array_value(hash, base_key, index, value, key = nil)
+      key_without_index = base_key.gsub("[#{index}]", '')
+      hash[key_without_index] ||= []
+
+      if key.nil?
+        hash[key_without_index][index] = value
+      else
+        hash[key_without_index][index] ||= {}
+        hash[key_without_index][index][key] = value
+      end
+    end
+
+    def set_deep_hash_positioned_value(new_hash, base_key, v, child_key)
+      index = array_index(base_key)
+
+      if index
+        set_deep_hash_array_value(new_hash, base_key, index, v, child_key)
+      else
+        set_deep_hash_value(new_hash, base_key, v, child_key)
+      end
+    end
+
+    def array_index(key)
+      match = key.match(/\[([^)]+)\]/)
+      if match
+        match[1].to_i
+      end
+    end
+
+    def set_deep_hash_value(hash, base_key, value, key = nil)
+      if key.nil?
+        hash[base_key] = value
+      else
+        hash[base_key] ||= {}
+        hash[base_key][key] = value
+      end
     end
   end
 end
