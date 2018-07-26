cd71ac890fb8e04486b9af131b4748dc26126081
darthjee <darthjee@gmail.com>
2018-07-26 09:19:03 +0200

    Use meaningfull names

--- lib/darthjee/core_ext/numeric.rb
+++ lib/darthjee/core_ext/numeric.rb
@@ -1,6 +1,6 @@
 class Numeric
-  def percent_of(n)
-    return Float::INFINITY if n == 0
-    (to_f / n.to_f) * 100.0
+  def percent_of(total)
+    return Float::INFINITY if total == 0
+    (to_f / total.to_f) * 100.0
   end
 end
