2aa0f388f0049e57b953bba12385aeef04e4ff00
darthjee <darthjee@gmail.com>
2018-07-30 11:29:00 +0200

    Add spec for to_hash_mapper

---
+++ spec/lib/hash/to_hash_mapper_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Hash::ToHashMapper do
+  it_behaves_like 'a hash with map_to_hash method' do
+    subject { described_class.new(hash) }
+    let(:mapped) { subject.map(&mapping_block) }
+  end
+end
