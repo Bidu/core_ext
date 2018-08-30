107c89ee2ff16729d30228448978c67b54dfc768
darthjee <darthjee@gmail.com>
2018-08-30 14:31:08 +0200

    Extract complex code

--- lib/darthjee/core_ext/hash/squasher.rb
+++ lib/darthjee/core_ext/hash/squasher.rb
@@ -2,18 +2,29 @@
 
 class Hash
   module Squasher
-    def self.squash(origin)
-      {}.tap do |hash|
-        origin.each do |key, value|
-          if value.is_a? Hash
-            value.squash.each do |k, v|
-              new_key = [key, k].join('.')
-              hash[new_key] = v
-            end
-          else
-            hash[key] = value
+    class Builder
+      attr_reader :key, :value
+
+      def initialize(key, value)
+        @value = value
+        @key = key
+      end
+
+      def to_h
+        if value.is_a? Hash
+          value.squash.inject({}) do |hash, (k, v)|
+            new_key = [key, k].join('.')
+            hash.merge!(new_key => v)
           end
+        else
+          { key => value }
         end
+      end
+    end
+
+    def self.squash(origin)
+      origin.inject({}) do |hash, (key, value)|
+        hash.merge!(Builder.new(key, value).to_h)
       end
     end
   end
