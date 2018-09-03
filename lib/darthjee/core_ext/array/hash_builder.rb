08a9afbf2553852fc94c434e79e5f03a46b84b5d
darthjee <darthjee@gmail.com>
2018-09-03 09:28:07 +0200

    Modularize Array

--- lib/darthjee/core_ext/array/hash_builder.rb
+++ lib/darthjee/core_ext/array/hash_builder.rb
@@ -1,28 +1,33 @@
 # frozen_string_literal: true
 
-class Array
-  class HashBuilder
-    attr_accessor :values, :keys
+module Darthjee
+  module CoreExt
+    module Array
+      class HashBuilder
+        attr_accessor :values, :keys
 
-    def initialize(values, keys)
-      @values = values.dup
-      @keys = keys.dup
-    end
+        def initialize(values, keys)
+          @values = values.dup
+          @keys = keys.dup
+        end
 
-    def build
-      fixes_sizes
+        def build
+          fixes_sizes
 
-      Hash[[keys, values].transpose]
-    end
+          ::Hash[[keys, values].transpose]
+        end
 
-    private
+        private
 
-    def fixes_sizes
-      values.concat Array.new(keys.size - values.size) if needs_resizing?
-    end
+        def fixes_sizes
+          return unless needs_resizing?
+          values.concat ::Array.new(keys.size - values.size)
+        end
 
-    def needs_resizing?
-      keys.size > values.size
+        def needs_resizing?
+          keys.size > values.size
+        end
+      end
     end
   end
 end
