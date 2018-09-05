1d608d7a3f92354f26025f2bd6f4b45e6d5841c0
darthjee <darthjee@gmail.com>
2018-09-05 09:33:08 +0200

    Modularize Object

--- lib/darthjee/core_ext/object.rb
+++ lib/darthjee/core_ext/object.rb
@@ -2,10 +2,18 @@
 
 require 'darthjee/core_ext/object/default_value'
 
-class Object
-  def is_any?(*classes)
-    classes.any? do |clazz|
-      is_a?(clazz)
+module Darthjee
+  module CoreExt
+    module Object
+      def is_any?(*classes)
+        classes.any? do |clazz|
+          is_a?(clazz)
+        end
+      end
     end
   end
+end
+
+class Object
+  include Darthjee::CoreExt::Object
 end
