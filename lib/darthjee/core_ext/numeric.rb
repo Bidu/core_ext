e510e6b380f0d5937fc1517ffa77781f84912168
darthjee <darthjee@gmail.com>
2018-09-05 09:31:27 +0200

    Modularize numeric

--- lib/darthjee/core_ext/numeric.rb
+++ lib/darthjee/core_ext/numeric.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-class Numeric
-  def percent_of(total)
-    return Float::INFINITY if total&.zero?
-    (to_f / total.to_f) * 100.0
+module Darthjee
+  module CoreExt
+    module Numeric
+      def percent_of(total)
+        return Float::INFINITY if total&.zero?
+        (to_f / total.to_f) * 100.0
+      end
+    end
   end
+end
+
+class Numeric
+  include Darthjee::CoreExt::Numeric
 end
