6891b90333e2d72a64f7f279a35277a38a2ce9ff
darthjee <darthjee@gmail.com>
2018-09-24 19:52:21 +0200

    Add documentation for hash builder

--- lib/darthjee/core_ext/array/hash_builder.rb
+++ lib/darthjee/core_ext/array/hash_builder.rb
@@ -3,14 +3,41 @@
 module Darthjee
   module CoreExt
     module Array
+      # Class responsible for building a Hash from 2 arrays
+      #
+      # @attribute [Array] values
+      #   values of the hash to be built
+      # @attribute [Array] keys
+      #   keys of the hash to be built
+      #
+      # @example
+      #   values = [10, 20, 30]
+      #   keys   = %i[a b c]
+      #   builder = Darthjee::CoreExt::Array::HashBuilder.new(values, keys)
+      #
+      #   builder.build  # returns { a: 10, b: 20, c: 30 }
+      #
+      # @example
+      #   hash = { a: 20, b: 200, c: 2000 }
+      #   builder = Darthjee::CoreExt::Array::HashBuilder.new(
+      #     hash.values,
+      #     hash.keys
+      #   )
+      #
+      #   builder.build == hash   # returns true
       class HashBuilder
         attr_accessor :values, :keys
 
+        # @param [Array] values List of values of the hash
+        # @param [Array] keys List of keys of the hash
         def initialize(values, keys)
           @values = values.dup
           @keys = keys.dup
         end
 
+        # Builds the hash
+        # @return [Hash] Hash whose keys and values are paired
+        #   from builder's keys and values
         def build
           fixes_sizes
 
