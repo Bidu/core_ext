97e2e3b527cb75e156f034de4087617f17cd3bda
darthjee <darthjee@gmail.com>
2018-09-25 17:26:41 +0200

    Add documentation to date

---
+++ spec/integration/yard/date/days_between_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Date do
+  describe 'yard' do
+    describe '#days_between' do
+      subject { Date.new(2018, 11, 21) }
+
+      context 'when checking against another date' do
+        let(:other_date) { Date.new(2019, 11, 21) }
+
+        it 'returns the days between' do
+          expect(subject.days_between(other_date)).to eq(365)
+        end
+      end
+
+      context 'when cheking agains a 4 years apart date' do
+        let(:other_date) { Date.new(2014, 11, 21) }
+
+        it 'returns the days between' do
+          expect(subject.days_between(other_date)).to eq(1461)
+        end
+      end
+
+      context 'when checking against time' do
+        let(:time) { Time.new(2017, 11, 21, 12, 0, 0) }
+
+        it 'ignores the hours' do
+          expect(subject.days_between(time)).to eq(365)
+        end
+      end
+    end
+  end
+end
