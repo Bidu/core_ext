207254fce037bf95cfbf9f04b1d1a93495924042
darthjee <darthjee@gmail.com>
2018-09-25 19:46:11 +0200

    Sort methods

--- lib/darthjee/core_ext/hash/transformable.rb
+++ lib/darthjee/core_ext/hash/transformable.rb
@@ -4,24 +4,24 @@ module Darthjee
   module CoreExt
     module Hash
       module Transformable
-        def squash
-          Hash::Squasher.squash(self)
-        end
-
-        def to_deep_hash(separator = '.')
-          Hash::DeepHashConstructor.new(separator).deep_hash(self)
-        end
-
-        def map_to_hash(&block)
-          Hash::ToHashMapper.new(self).map(&block)
-        end
-
         def exclusive_merge(hash)
           dup.exclusive_merge!(hash)
         end
 
         def exclusive_merge!(hash)
           merge!(hash.slice(*keys))
+        end
+
+        def map_to_hash(&block)
+          Hash::ToHashMapper.new(self).map(&block)
+        end
+
+        def squash
+          Hash::Squasher.squash(self)
+        end
+
+        def to_deep_hash(separator = '.')
+          Hash::DeepHashConstructor.new(separator).deep_hash(self)
         end
       end
     end
