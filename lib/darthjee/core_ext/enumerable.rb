3bfa1fa19affbd800cbd78ba5fcf4f1287f3f3b5
darthjee <darthjee@gmail.com>
2018-10-04 17:46:28 +0200

    Add enum docs

--- lib/darthjee/core_ext/enumerable.rb
+++ lib/darthjee/core_ext/enumerable.rb
@@ -32,6 +32,31 @@ module Enumerable
     end
   end
 
+  # Maps the elements into a new value, returning
+  # the first element that is evaluated to true
+  #
+  # This method is equivalent to #map#find but
+  # only calling the map block up to when a value
+  # is found
+  #
+  # @yield (*args) mappig block
+  #
+  # @example Using an array of keys to remove remove elements of a hash
+  #
+  #   service_map = {
+  #     a: nil,
+  #     b: false,
+  #     c: 'found me',
+  #     d: nil,
+  #     e: 'didnt find me'
+  #   }
+  #
+  #   keys = %i[a b c d e]
+  #
+  #   keys.map_and_find { |key| service_values.delete(key) }
+  #               # returns 'found me'
+  #   service_map # has lost only 3 keys returning
+  #               # { d: nil, e: 'didnt find me' }
   def map_and_find
     mapped = nil
     find do |*args|
@@ -40,6 +65,25 @@ module Enumerable
     mapped || nil
   end
 
+  # Maps the elements into a new value returning an
+  # array of the values mapped to non false values
+  #
+  # This method is equivalent to call #map#select
+  #
+  # @yield (*args) mapping block
+  #
+  # @example Mapping the values of hash to their size
+  #   hash = {
+  #     a: nil,
+  #     b: 'aka',
+  #     c: 'a'
+  #   }
+  #
+  #   values = hash.map_and_select do |key, value|
+  #     value && value.to_s
+  #   end
+  #
+  #   values # returns [3, 1]
   def map_and_select
     mapped = map do |*args|
       yield(*args)
@@ -47,6 +91,15 @@ module Enumerable
     mapped.select { |e| e }
   end
 
+  # Maps values and creates a hash whose values are
+  # the result of the #map and the keys are the original values
+  #
+  # @yield (*args) the mapping block
+  #
+  # @example Mapping strings to their sizes
+  #   strings =  %w[word big_word]
+  #
+  #   strings.map_to_hash(&:size) # returns { 'word' => 4, 'big_word' => 8 }
   def map_to_hash
     {}.tap do |hash|
       each do |element|
--- spec/integration/yard/enumerable_spec.rb
+++ spec/integration/yard/enumerable_spec.rb
@@ -3,30 +3,81 @@
 require 'spec_helper'
 
 describe Enumerable do
-  describe '#clean!' do
-    context 'when subject is a hash' do
-      subject(:hash) do
-        {
-          keep: 'value',
-          nil_value: nil,
-          empty_array: [],
-          empty_string: '',
-          empty_hash: {}
-        }
+  describe 'yard' do
+    describe '#clean!' do
+      context 'when subject is a hash' do
+        subject(:hash) do
+          {
+            keep: 'value',
+            nil_value: nil,
+            empty_array: [],
+            empty_string: '',
+            empty_hash: {}
+          }
+        end
+
+        it 'removes empty values' do
+          expect { hash.clean! }.to change { hash }.to(keep: 'value')
+        end
       end
 
-      it 'removes empty values' do
-        expect { hash.clean! }.to change { hash }.to(keep: 'value')
+      context 'when subject is an array' do
+        subject(:array) do
+          ['value', nil, [], '', {}]
+        end
+
+        it 'removes empty values' do
+          expect { array.clean! }.to change { array }.to(['value'])
+        end
       end
     end
 
-    context 'when subject is an array' do
-      subject(:array) do
-        ['value', nil, [], '', {}]
+    describe '#map_and_find' do
+      subject(:keys) { %i[a b c d e] }
+
+      let(:service_values) do
+        {
+          a: nil,
+          b: false,
+          c: 'found me',
+          d: nil,
+          e: 'didnt find me'
+        }
       end
 
-      it 'removes empty values' do
-        expect { array.clean! }.to change { array }.to(['value'])
+      it 'returns the first non false value' do
+        value = keys.map_and_find { |key| service_values.delete(key) }
+        expect(value).to eq('found me')
+      end
+
+      it 'stops running when value is found' do
+        expect do
+          keys.map_and_find { |key| service_values.delete(key) }
+        end.to change { service_values }.to(d: nil, e: 'didnt find me')
+      end
+    end
+
+    describe '#map_and_select' do
+      subject(:hash) do
+        {
+          a: nil,
+          b: 'aka',
+          c: 'a'
+        }
+      end
+
+      it 'returns the first non false value' do
+        array = hash.map_and_select { |_key, value| value&.size }
+        expect(array).to eq([3, 1])
+      end
+    end
+
+    describe 'map_to_hash' do
+      subject(:strings) { %w[word big_word] }
+
+      it 'returns a hash with the mapped values' do
+        hash = strings.map_to_hash(&:size)
+        expect(hash).to eq('word' => 4, 'big_word' => 8)
       end
     end
   end
